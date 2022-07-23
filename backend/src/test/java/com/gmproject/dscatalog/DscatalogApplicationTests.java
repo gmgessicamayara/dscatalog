5743eb2e8fe96548e233cdd8e788e4c1160390c2
Géssica Mendonça <gm.gessica@outlook.com>
2022-07-23 12:42:59 -0300

    backend created

---
+++ backend/src/test/java/com/gmproject/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmproject.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
