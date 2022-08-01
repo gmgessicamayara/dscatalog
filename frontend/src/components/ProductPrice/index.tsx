88e982783db0dad8b23259cbf38c529cb71fbfce
Géssica Mendonça <gm.gessica@outlook.com>
2022-08-01 20:20:36 -0300

    Product and ProductPrice

---
+++ frontend/src/components/ProductPrice/index.tsx
@@ -0,0 +1,12 @@
+import './style.css';
+
+const ProductPrice = () => {
+    return (
+        <div className="product-price-container">
+            <span>R$</span>
+               <h3>986.39</h3> 
+        </div> 
+    );
+}
+
+export default ProductPrice;
