6b6ffb2ea3ef884e5435f0035f51ea306dbffa91
Géssica Mendonça <gm.gessica@outlook.com>
2022-10-17 20:39:16 -0300

    Conditional styling

--- frontend/src/pages/Admin/Auth/Login/index.tsx
+++ frontend/src/pages/Admin/Auth/Login/index.tsx
@@ -35,6 +35,7 @@ const Login = () => {
           Erro ao tentar efetuar o login.
         </div>
       )}
+      
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input
@@ -46,7 +47,8 @@ const Login = () => {
               }
             })}
             type="text"
-            className="form-control base-input"
+            className={`form-control base-input ${errors.username
+            ? 'is-invalid' : '' }`}
             placeholder="Email"
             name="username"
           />
@@ -60,7 +62,8 @@ const Login = () => {
               required: 'Campo obrigatório'
              })}
             type="password"
-            className="form-control base-input "
+            className={`form-control base-input ${errors.password
+              ? 'is-invalid' : '' }`}
             placeholder="Password"
             name="password"
           />
